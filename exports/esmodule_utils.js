4052d59f1ab12b0a9a3db078f9931e85c1dd6c0c
YieBonface <Bonface>
2024-12-08 18:50:03 +0300

    setting up server , module es exports and adding gitignore

---
+++ exports/esmodule_utils.js
@@ -0,0 +1,15 @@
+const posts = [
+  { id: 1, title: "Post 1", content: "This is the first post" },
+  { id: 2, title: "Post 2", content: "This is the second post" },
+];
+
+const getpostsList = () => {
+  return posts;
+};
+const getpostsListByIndex = (index) => {
+  return posts[index];
+};
+
+export default getpostsListByIndex;
+
+export { getpostsList };
